4923cefee0819a0468b30e2cafd581f7807be60d
Eduardo Fronza <edujfronza@gmail.com>
2023-10-17 17:12:20 -0300

    feature - implementando a segunda oppção de botão

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,25 +1,31 @@
-
-
 export type ButtonProps = {
-    children: React.ReactNode
+    children: React.ReactNode;
+    variant?: 'primary' | 'secondary' | 'tertiary'; 
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, className, disabled, ...res}: ButtonProps) => {
-
-    const generalStyle = ' rounded-md  px-6 py-2 ';
-    const Btn = (classes: string) => {
-        return ( 
-            <button 
-                className={`${generalStyle} ${classes} ${className}`}
-                disabled={disabled}
-                {...res}
-            >
-                {children}
-            </button>
-        )
+    
+function getVariant(variant: ButtonProps['variant'], disabled: ButtonProps['disabled']) {
+    switch(variant) {
+        case 'primary':
+            return disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white'
+        case 'secondary':
+            return disabled ? 'bg-disabled text-disabled' : 'bg-quaternary text-primary'
+        case 'tertiary':
+        default:
+            return disabled ? '' : ''
     }
+}
 
-    return Btn(disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white')
+const Button = ({ variant = 'primary', children, className, disabled, ...res}: ButtonProps) => {
+    return ( 
+        <button 
+            className={` rounded-md  px-6 py-2 ${getVariant(variant, disabled)} ${className}`}
+            disabled={disabled}
+            {...res}
+        >
+            {children}
+        </button>
+    )
 }
 
 export default Button
